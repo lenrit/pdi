e9c9c4de5768da3b6ec55ff7ed5036043dc43577
martin <164651909+lenrit@users.noreply.github.com>
2024-05-23 23:07:54 -0300

    Add files via upload

--- 16-05/index.js
+++ 16-05/index.js
@@ -1,32 +1,27 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-// app.listen(3000, function(){
-//     console.log("Funciona :D")
-// })
-
-app.get("/", (req, res) => {
-  res.send("Marco Gonzalez")
-})
+const express=require('express');
+const app=express();
+const port=3000;
 
 lista_usuarios = [
     {nombre: "Marco", id:"34", password:"dell"}, 
     {nombre:"Johana", id:"7655", password:"locura"}, 
-    {nombre:"Martin", id:"8008135", password:"node"}]
-    
-// const usuariosRouter = require('./routes/usuarios');
-// app.use("/usuarios", usuariosRouter);
+    {nombre:"Martin", id:"8008135", password:"nodemon"}
+]
+//Inicio (log in y sign up)
+app.get("/",(request,response)=>{
+    response.sendFile("F:/pdi/intento en clase/inicio.html");
+});
+//Inicio
+app.get("/home",(req,res)=>{
+    res.sendFile("F:/pdi/intento en clase/home.html");
+})
+//buscar tabla por comerciante
+app.get("/comerciante",(req,res)=>{
+    const name=req.query.username;
+    const usuario =lista_usuarios.filter(usuario => usuario.nombre == name);
+    res.send(usuario);
+})
 
-app.get("/usuarios",(req,res)=>{
-    res.send(lista_usuarios);
-})
-app.get("/usuarios/:id", (req, res) => {
-    const id = (req.params.id);
-    const usuario =lista_usuarios.filter(usuario => usuario.id == id)
-    res.json({
-        usuario
-})
-})
 app.listen(port, () => {
-  console.log(`Servidor funcionando en el puerto ${port}`)
+    console.log(`Servidor funcionando en el puerto ${port}`)
 })
