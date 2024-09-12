ca266d94f1a38621bae233b5b26f7549115d7283
Johana Galeano <163671250+johanagaleanoo@users.noreply.github.com>
2024-09-12 20:05:56 -0300

    Delete server directory

--- server/rutas/user_routes.js
+++
@@ -1,9 +0,0 @@
-const express= require ("express");
-const router= express.Router ();
-const usuario_controlador= require ("../controladores/usuario_controlador");
-router.get ('/usuarios', usuario_controlador.obtener_usuarios);
-router.get ('/usuarios/:id', usuario_controlador.obtener_id);
-router.get ('/usuarios/insert', usuario_controlador.insertar_usuarios);
-router.get ('/usuarios/delete', usuario_controlador.delete_usuarios);
-router.get ('/usuarios/update', usuario_controlador.update_usuarios);
-module.exports= router;
