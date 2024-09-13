239333a3b0fb1ae57e9dfda0970a7ca388780326
diegohpezet <diegohpezet@gmail.com>
2024-09-12 21:15:30 -0300

    correcciones en clase

--- server/config/data_base.js
+++ server/config/data_base.js
@@ -1,13 +1,13 @@
-const mysql= require ('mysql2');
-const { Sequelize }= require ('sequelize');
+require('dotenv').config()
+const mysql = require('mysql2');
+const { Sequelize } = require('sequelize');
 
 /**
  * bases de datos
  */
-const sequelize= new Sequelize (process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql2',
+    dialect: 'mysql',
 });
 
-module.exports= sequelize;
+module.exports = sequelize;
