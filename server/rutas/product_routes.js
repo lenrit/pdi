4f9200b409f272b91539470238d0be6c64bdb0c4
Johana Galeano <163671250+johanagaleanoo@users.noreply.github.com>
2024-09-19 11:55:09 -0300

    Update product_routes.js

--- server/rutas/product_routes.js
+++ server/rutas/product_routes.js
@@ -7,8 +7,8 @@ const producto_controlador= require ("../controladores/productos_controlador");
  */
 router.get ('/productos', producto_controlador.obtener_productos);
 router.get ('/productos/:id', producto_controlador.obtener_id_productos);
-router.get ('/productos/insert', producto_controlador.insertar_productos);
-router.get ('/productos/update', producto_controlador.update_productos);
-router.get ('/productos/delete', producto_controlador.delete_productos);
+router.post ('/productos/insert', producto_controlador.insertar_productos);
+router.put ('/productos/update/:id', producto_controlador.update_productos);
+router.delete ('/productos/delete/:id', producto_controlador.delete_productos);
 
 module.exports= router;
